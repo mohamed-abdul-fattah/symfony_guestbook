8df035b61a087c222cd4a70bcaf521c8bef3b278
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-10-17 10:18:43 +0300

    Removed reduntant homepage query

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -4,23 +4,17 @@ namespace App\Controller;
 
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
-use App\Repository\ConferenceRepository;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
     #[Route('/', name: 'homepage')]
-    public function index(ConferenceRepository $conferenceRepository): Response
+    public function index(): Response
     {
-        return $this->render(
-            'conference/index.html.twig',
-            [
-                'conferences' => $conferenceRepository->findAll()
-            ]
-        );
+        return $this->render('conference/index.html.twig');
     }
 
     #[Route('/conference/{id}', name: 'conference')]
