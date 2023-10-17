e4db1b1967d5e8d018bd048fa60f8cb134bf9675
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-10-17 15:20:48 +0300

    Added comment feedback form

--- src/Controller/Admin/CommentCrudController.php
+++ src/Controller/Admin/CommentCrudController.php
@@ -9,6 +9,7 @@ use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\EmailField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Filter\EntityFilter;
@@ -40,7 +41,10 @@ class CommentCrudController extends AbstractCrudController
         yield TextField::new('author');
         yield EmailField::new('email');
         yield TextareaField::new('text')->hideOnIndex();
-        yield TextField::new('photoFilename')->hideOnIndex();
+        yield ImageField::new('photoFilename')
+            ->setBasePath('/uploads/photos')
+            ->setLabel('Photo')
+            ->onlyOnIndex();
 
         $createdAt = DateTimeField::new('createdAt')->setFormTypeOptions([
             'years'  => range(date('Y'), date('Y') + 5),
--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Form\CommentType;
 use App\Entity\Conference;
 use App\Repository\CommentRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,23 +15,49 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class ConferenceController extends AbstractController
 {
-    #[Route('/', name: 'homepage')]
+    public function __construct(private readonly EntityManagerInterface $entityManager)
+    {}
+
+    #[Route('/', name: 'homepage', methods: ['GET'])]
     public function index(): Response
     {
         return $this->render('conference/index.html.twig');
     }
 
     #[Route('/conference/{slug}', name: 'conference')]
-    public function show(Request $request, Conference $conference, CommentRepository $commentRepository): Response
+    public function show(
+        Request $request,
+        Conference $conference,
+        CommentRepository $commentRepository,
+        #[Autowire('%photo_dir%')] string $photoDir,
+    ): Response
     {
         $offset    = max(0, $request->query->get('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($conference, $offset);
+        $comment   = new Comment();
+        $form      = $this->createForm(CommentType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setConference($conference);
+            if ($photo = $form['photo']->getData()) {
+                $filename = bin2hex(random_bytes(6)).'.'.$photo->guessExtension();
+                $photo->move($photoDir, $filename);
+                $comment->setPhotoFilename($filename);
+            }
+
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
+        }
 
         return $this->render('conference/show.html.twig', [
-            'conference' => $conference,
-            'comments'   => $paginator,
-            'previous'   => $offset - CommentRepository::PAGINATOR_PER_PAGE,
-            'next'       => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'conference'   => $conference,
+            'comments'     => $paginator,
+            'previous'     => $offset - CommentRepository::PAGINATOR_PER_PAGE,
+            'next'         => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form,
         ]);
     }
 }
