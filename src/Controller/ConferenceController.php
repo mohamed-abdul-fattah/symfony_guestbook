3ad326ea701349f3c89b0315cc0a1ba0e0c3cbc9
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-10-26 11:22:48 +0300

    Async comment spam checks

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -5,18 +5,26 @@ namespace App\Controller;
 use App\Entity\Comment;
 use App\Form\CommentType;
 use App\Entity\Conference;
+use App\Message\CommentMessage;
 use App\Repository\CommentRepository;
-use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 
 class ConferenceController extends AbstractController
 {
-    public function __construct(private readonly EntityManagerInterface $entityManager)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly MessageBusInterface $bus,
+    )
     {}
 
     #[Route('/', name: 'homepage', methods: ['GET'])]
@@ -25,12 +33,18 @@ class ConferenceController extends AbstractController
         return $this->render('conference/index.html.twig');
     }
 
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     * @throws \Exception
+     */
     #[Route('/conference/{slug}', name: 'conference')]
     public function show(
         Request $request,
         Conference $conference,
         CommentRepository $commentRepository,
-        SpamChecker $spamChecker,
         #[Autowire('%photo_dir%')] string $photoDir,
     ): Response
     {
@@ -49,6 +63,7 @@ class ConferenceController extends AbstractController
             }
 
             $this->entityManager->persist($comment);
+            $this->entityManager->flush();
 
             // Checking for spam
             $context = [
@@ -57,11 +72,7 @@ class ConferenceController extends AbstractController
                 'referrer' => $request->headers->get('referrer'),
                 'permalink' => $request->getUri(),
             ];
-            if (SpamChecker::BLATANT_SPAM === $spamChecker->getSpamScore($comment, $context)) {
-                throw new \RuntimeException('Blatant spam, go away!');
-            }
-
-            $this->entityManager->flush();
+            $this->bus->dispatch(new CommentMessage($comment->getId(), $context));
 
             return $this->redirectToRoute('conference', ['slug' => $conference->getSlug()]);
         }
---
+++ src/MessageHandler/CommentMessageHandler.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\MessageHandler;
+
+use App\Message\CommentMessage;
+use App\Repository\CommentRepository;
+use App\Service\SpamChecker;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\RedirectionExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\ServerExceptionInterface;
+use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
+
+#[AsMessageHandler]
+class CommentMessageHandler
+{
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SpamChecker            $spamChecker,
+        private readonly CommentRepository      $commentRepository,
+    ) {
+    }
+
+    /**
+     * @throws TransportExceptionInterface
+     * @throws ServerExceptionInterface
+     * @throws RedirectionExceptionInterface
+     * @throws ClientExceptionInterface
+     */
+    public function __invoke(CommentMessage $message): void
+    {
+        $comment = $this->commentRepository->find($message->getId());
+        if (!$comment) {
+            return;
+        }
+
+        if (SpamChecker::BLATANT_SPAM === $this->spamChecker->getSpamScore($comment, $message->getContext())) {
+            $comment->setState('spam');
+        } else {
+            $comment->setState('published');
+        }
+
+        $this->entityManager->flush();
+    }
+}
