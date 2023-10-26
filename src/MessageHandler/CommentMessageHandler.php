35002c1fbe978b11b5bce8c103b79eee6b4129e6
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-10-26 17:56:43 +0300

    Send mails

--- src/MessageHandler/CommentMessageHandler.php
+++ src/MessageHandler/CommentMessageHandler.php
@@ -7,6 +7,9 @@ use App\Repository\CommentRepository;
 use App\Service\SpamChecker;
 use Doctrine\ORM\EntityManagerInterface;
 use Psr\Log\LoggerInterface;
+use Symfony\Bridge\Twig\Mime\NotificationEmail;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Messenger\Attribute\AsMessageHandler;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Workflow\WorkflowInterface;
@@ -19,12 +22,14 @@ use Symfony\Contracts\HttpClient\Exception\TransportExceptionInterface;
 class CommentMessageHandler
 {
     public function __construct(
-        private readonly EntityManagerInterface $entityManager,
-        private readonly SpamChecker            $spamChecker,
-        private readonly CommentRepository      $commentRepository,
-        private readonly MessageBusInterface    $bus,
-        private readonly WorkflowInterface      $commentStateMachine,
-        private readonly LoggerInterface        $logger,
+        private readonly EntityManagerInterface              $entityManager,
+        private readonly SpamChecker                         $spamChecker,
+        private readonly CommentRepository                   $commentRepository,
+        private readonly MessageBusInterface                 $bus,
+        private readonly WorkflowInterface                   $commentStateMachine,
+        private readonly LoggerInterface                     $logger,
+        private readonly MailerInterface                     $mailer,
+        #[Autowire('%admin_email%')] private readonly string $adminEmail,
     ) {
     }
 
@@ -32,7 +37,7 @@ class CommentMessageHandler
      * @throws TransportExceptionInterface
      * @throws ServerExceptionInterface
      * @throws RedirectionExceptionInterface
-     * @throws ClientExceptionInterface
+     * @throws ClientExceptionInterface|\Symfony\Component\Mailer\Exception\TransportExceptionInterface
      */
     public function __invoke(CommentMessage $message): void
     {
@@ -52,8 +57,14 @@ class CommentMessageHandler
             $this->entityManager->flush();
             $this->bus->dispatch($message);
         } else if ($this->commentStateMachine->can($comment, 'publish') || $this->commentStateMachine->can($comment, 'publish_ham')) {
-            $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
-            $this->entityManager->flush();
+//            $this->commentStateMachine->apply($comment, $this->commentStateMachine->can($comment, 'publish') ? 'publish' : 'publish_ham');
+//            $this->entityManager->flush();
+            $this->mailer->send((New NotificationEmail())
+                ->subject('New comment has been posted')
+                ->htmlTemplate('emails/comment_notification.html.twig')
+                ->to($this->adminEmail)
+                ->context(['comment' => $comment])
+            );
         } else {
             $this->logger->debug('Dropping comment message', [
                 'comment' => $comment->getId(),
