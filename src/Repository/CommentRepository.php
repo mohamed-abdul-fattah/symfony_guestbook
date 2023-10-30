c2b5f1ada2df2e7ff1b623a9acbff3341583ec06
Mohamed Abdul-Fattah <csmohamed8@gmail.com>
2023-10-30 09:37:20 +0200

    Delete old rejected/spam comments

--- src/Repository/CommentRepository.php
+++ src/Repository/CommentRepository.php
@@ -5,6 +5,9 @@ namespace App\Repository;
 use App\Entity\Comment;
 use App\Entity\Conference;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\ORM\NonUniqueResultException;
+use Doctrine\ORM\NoResultException;
+use Doctrine\ORM\QueryBuilder;
 use Doctrine\ORM\Tools\Pagination\Paginator;
 use Doctrine\Persistence\ManagerRegistry;
 
@@ -18,11 +21,35 @@ use Doctrine\Persistence\ManagerRegistry;
  */
 class CommentRepository extends ServiceEntityRepository
 {
+    private const DAYS_BEFORE_REJECTED_REMOVAL = 7;
+
     public const PAGINATOR_PER_PAGE = 2;
 
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Comment::class);
+    }
+
+    /**
+     * @throws NonUniqueResultException
+     * @throws NoResultException
+     * @throws \Exception
+     */
+    public function countOldRejected(): int
+    {
+        return $this
+            ->getOldRejectedQueryBuilder()
+            ->select('COUNT(c.id)')
+            ->getQuery()
+            ->getSingleScalarResult();
+    }
+
+    /**
+     * @throws \Exception
+     */
+    public function deleteOldRejected(): int
+    {
+        return $this->getOldRejectedQueryBuilder()->delete()->getQuery()->execute();
     }
 
     public function getCommentPaginator(Conference $conference, int $offset): Paginator
@@ -38,5 +65,20 @@ class CommentRepository extends ServiceEntityRepository
             ->getQuery();
 
         return new Paginator($query);
+    }
+
+    /**
+     * @throws \Exception
+     */
+    private function getOldRejectedQueryBuilder(): QueryBuilder
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.state = :rejected_state or c.state = :spam_state')
+            ->andWhere('c.createdAt < :date')
+            ->setParameters([
+                'rejected_state' => 'rejected',
+                'spam_state'     => 'spam',
+                'date'           => new \DateTimeImmutable(- self::DAYS_BEFORE_REJECTED_REMOVAL.' days'),
+            ]);
     }
 }
